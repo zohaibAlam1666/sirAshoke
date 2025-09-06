cbf0cecf62eb0093ed958d7b6f41c8681b59054b
ARajamani80 <helash08@hotmail.com>
2025-09-06 16:48:31 +0800

    new file added

--- practice/main.py
+++ practice/main.py
@@ -1,1 +1,27 @@
-print("hello")
+
+
+
+
+class student:
+    def __init__(self, rollId, name, age):
+        self.rollId = rollId
+        self.name = name
+        self.age = age
+
+    def greet(self):
+
+        print (f"user name is :{self.age}, {self.rollId},{self.name}")
+
+    def test(self, name):
+        return name
+    
+        
+
+student12 = student (2343, "ashok",35)
+student12.greet()
+
+student45 = student (2454, "Zohaib", 25)
+student45.greet()
+
+student98 = student (34343, "alam", 35)
+print(student98.test("alam"))

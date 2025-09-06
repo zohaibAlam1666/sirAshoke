e50f1ce899b60394530a197150f725077c1d035d
zohaibAlam1666 <azayn031@gmail.com>
2025-09-06 14:02:58 +0500

    jj

--- practice/main.py
+++ practice/main.py
@@ -2,26 +2,43 @@
 
 
 
-class student:
-    def __init__(self, rollId, name, age):
-        self.rollId = rollId
-        self.name = name
-        self.age = age
+# class student:
+#     def __init__(self, rollId, name, age):
+#         self.rollId = rollId
+#         self.name = name
+#         self.age = age
 
-    def greet(self):
+#     def greet(self):
 
-        print (f"user name is :{self.age}, {self.rollId},{self.name}")
+#         print (f"user name is :{self.age}, {self.rollId},{self.name}")
 
-    def test(self, name):
-        return name
+#     def test(self, name):
+#         return name
     
         
 
-student12 = student (2343, "ashok",35)
-student12.greet()
+# student12 = student (2343, "ashok",35)
+# student12.greet()
 
-student45 = student (2454, "Zohaib", 25)
-student45.greet()
+# student45 = student (2454, "Zohaib", 25)
+# student45.greet()
 
-student98 = student (34343, "alam", 35)
-print(student98.test("alam"))
+# student98 = student (34343, "alam", 35)
+# print(student98.test("alam"))
+
+
+
+
+
+class Bankaccount:
+    def __init__(self, ownerid, balance=0):
+
+        self.owner = ownerid
+        self.__balance = balance
+
+
+acc = Bankaccount("ali", 1000)
+
+print(acc.owner)
+
+print(acc.__balance)
